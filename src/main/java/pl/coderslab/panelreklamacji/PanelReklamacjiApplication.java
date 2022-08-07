8bb2036d9e0a401e721705a92b72904dfba474f1
Kuba <jakub.kucinski@icloud.com>
2022-08-07 21:43:57 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/panelreklamacji/PanelReklamacjiApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.panelreklamacji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelReklamacjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanelReklamacjiApplication.class, args);
+    }
+
+}
